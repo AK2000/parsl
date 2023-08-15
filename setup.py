99abc85de4115058f6806c4721b0f764915f4d41
Alok Kamatar <alokvk2@uchicago.edu>
2023-08-14 21:38:38 -0500

    Removing energy monitor from setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
     python_requires=">=3.8.0",
     install_requires=install_requires,
     scripts = ['parsl/executors/high_throughput/process_worker_pool.py',
-               'parsl/monitoring/energy/process_energy_monitor.py',
                'parsl/executors/workqueue/exec_parsl_function.py',
                'parsl/executors/workqueue/parsl_coprocess.py',
     ],
